90e623c2a252d1d4edcecc4cbdfab9960f6d138b
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-03-09 11:04:33 -0300

    feat(sls): inserting serverless lambda on function

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gabrielmatuk/wppgpt/src/config"
 )
 
@@ -86,10 +90,45 @@ func GenerateGPTText(query string) (string, error) {
 	return resp.Choices[0].Message.Content, nil
 }
 
-func process(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func parseBase64RequestData(r string) (string, error) {
+	dataBytes, err := base64.StdEncoding.DecodeString(r)
+	if err != nil {
+		return "", err
+	}
 
+	data, err := url.ParseQuery(string(dataBytes))
+	if err != nil {
+		return "", err
+	}
+	if data.Has("Body") {
+		return data.Get("Body"), nil
+	}
+	return "", errors.New("body not found")
+}
+
+func process(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	result, err := parseBase64RequestData(request.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, nil
+	}
+
+	text, err := GenerateGPTText(result)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, nil
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       text,
+	}, nil
 }
 
 func main() {
+	lambda.Start(process)
 	fmt.Println("Key" + config.ApiKey)
 }
