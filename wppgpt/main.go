663e43ddd1c32ede5059ebc32c11fcf450732537
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-03-09 10:27:26 -0300

    misc(init): creating type structures

--- wppgpt/main.go
+++ wppgpt/main.go
@@ -1,1 +1,12 @@
 package main
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Request struct {
+	Model     string    `json:"model"`
+	Messages  []Message `json:"messages"`
+	MaxTokens int       `json:"max_tokens"`
+}
